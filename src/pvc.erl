0ce6edfb727ab722ee2dfd3d223ab93c85432cd7
ergl <ergl@users.noreply.github.com>
2019-02-08 02:47:15 +0100

    Implement single and multi read
    Also add type aliases for errors

--- src/pvc.erl
+++ src/pvc.erl
@@ -47,6 +47,13 @@
 -type partition_ws() :: orddict:orddict(term(), term()).
 -type ws() :: orddict:orddict(index_node(), partition_ws()).
 
+%% FIXME(borja): Disparity with Antidote
+%% ordsets vs sets (what's more efficient on the wire?)
+%% Antidote stores _only_ partition ids, we store index nodes {partition_id, node_ip}
+-type read_partitions() :: sets:set(index_node()).
+
+-type vc() :: pvc_vclock:vc(index_node()).
+
 -record(tx_state, {
     %% Identifier of the current transaction
     %% Must be unique among all other active transactions
@@ -57,16 +64,24 @@
     %% Write set of the current transaction, partitioned
     %% by partition. This way, we can send to partitions
     %% only the subset they need to verify
-    writeset = [] :: ws()
+    writeset = [] :: ws(),
+    %% VC representing the read versions
+    vc_dep = pvc_vclock:new() :: vc(),
+    %% VC representing the max threshold of version that should be read
+    vc_aggr = pvc_vclock:new() :: vc(),
+    %% What partitions have we read before
+    read_partitions = sets:new() :: read_partitions()
 }).
 
 -opaque connection() :: #conn{}.
 -opaque transaction() :: #tx_state{}.
--type abort_reason() :: atom().
+-type socket_error() :: {error, inet:posix()}.
+-type abort() :: {abort, atom()}.
 
 -export_type([connection/0,
               transaction/0,
-              abort_reason/0]).
+              abort/0,
+              socket_error/0]).
 
 -define(missing, erlang:error(not_implemented)).
 
@@ -75,7 +90,8 @@
 %%====================================================================
 
 -spec connect(node_ip(), inet:port_number()) -> {ok, connection()}
-                                                | {error, inet:posix()}.
+                                              | socket_error().
+
 connect(Address, Port) ->
     case gen_tcp:connect(Address, Port, ?conn_options) of
         {error, Reason} ->
@@ -85,7 +101,8 @@ connect(Address, Port) ->
     end.
 
 -spec connect_1(inet:hostname(), inet:port_number(), inet:socket()) -> {ok, connection()}
-                                                                     | {error, inet:posix()}.
+                                                                     | socket_error().
+
 connect_1(ConnectedTo, Port, Socket) ->
     ok = gen_tcp:send(Socket, ppb_protocol_driver:connect()),
     case gen_tcp:recv(Socket, 0) of
@@ -104,13 +121,20 @@ connect_1(ConnectedTo, Port, Socket) ->
 start_transaction(#conn{self_ip=IP}, Id) ->
     {ok, #tx_state{id={IP, Id}}}.
 
+%% @doc Read a key, or a list of keys.
+%%
+%%      If given a list of keys, will return a list of values,
+%%      in the same order as the original keys.
+%%
 -spec read(connection(), transaction(), any()) -> {ok, any(), transaction()}
-                                                | {error, abort_reason()}.
-read(_Conn, _Tx, Keys) when is_list(Keys) ->
-    ?missing;
+                                                | abort()
+                                                | socket_error().
 
-read(_Conn, _Tx, _Key) ->
-    ?missing.
+read(Conn, Tx, Keys) when is_list(Keys) ->
+    catch read_batch(Conn, Keys, [], Tx);
+
+read(Conn, Tx, Key) ->
+    read_internal(Key, Conn, Tx).
 
 %% @doc Update the given Key. Old value is replaced with new one
 -spec update(connection(), transaction(), any(), any()) -> {ok, transaction()}.
@@ -126,7 +150,7 @@ update(Conn, Tx = #tx_state{writeset=WS}, Updates) when is_list(Updates) ->
     end, WS, Updates),
     {ok, Tx#tx_state{read_only=false, writeset=NewWS}}.
 
--spec commit(connection(), transaction()) -> ok | {error, abort_reason()}.
+-spec commit(connection(), transaction()) -> ok | abort().
 commit(_Conn, #tx_state{read_only=true}) ->
     ok;
 
@@ -140,7 +164,7 @@ close(#conn{sockets=Sockets}) ->
     end, ok, Sockets).
 
 %%====================================================================
-%% Internal functions
+%% Connect Internal functions
 %%====================================================================
 
 %% @doc Get our own IP from a given socket
@@ -207,6 +231,10 @@ open_remote_sockets_1(Nodes, SelfNode, Port, Sockets) ->
         end
     end, Sockets, Nodes).
 
+%%====================================================================
+%% Routing Internal functions
+%%====================================================================
+
 -spec get_key_indexnode(connection(), term()) -> index_node().
 get_key_indexnode(#conn{ring = {Size, Layout}}, Key) ->
     Pos = convert_key(Key) rem Size + 1,
@@ -228,6 +256,96 @@ convert_key(Key) when is_integer(Key) ->
 convert_key(TermKey) ->
     HashedKey = crypto:hash(sha, term_to_binary({<<"antidote">>, term_to_binary(TermKey)})),
     abs(crypto:bytes_to_integer(HashedKey)).
+
+%%====================================================================
+%% Read Internal functions
+%%====================================================================
+
+%% @doc Accumulatevly read the given keys.
+%%
+%%      On any error, return inmediately and stop reading further keys.
+%%      Will not return any read value in that case.
+%%
+-spec read_batch(connection(), [term()], [term()], transaction()) -> {ok, [term()], transaction()}
+                                                                   | abort()
+                                                                   | socket_error().
+
+read_batch(_, [], ReadAcc, AccTx) ->
+    {ok, lists:reverse(ReadAcc), AccTx};
+
+read_batch(Conn, [Key | Rest], ReadAcc, AccTx) ->
+    case read_internal(Key, Conn, AccTx) of
+        {ok, Value, NewTx} ->
+            read_batch(Conn, Rest, [Value | ReadAcc], NewTx);
+        {abort, _}=Abort ->
+            throw(Abort);
+        {error, _}=Err ->
+            throw(Err)
+    end.
+
+-spec read_internal(term(), connection(), transaction()) -> {ok, term(), transaction()}
+                                                          | abort()
+                                                          | socket_error().
+
+read_internal(Key, Conn=#conn{sockets=Sockets}, Tx=#tx_state{writeset=WS}) ->
+    case key_updated(Conn, Key, WS) of
+        {ok, Value} ->
+            {ok, Value, Tx};
+        {false, {_, Node}=IndexNode} ->
+            Socket = orddict:fetch(Node, Sockets),
+            remote_read(IndexNode, Socket, Key, Tx)
+    end.
+
+-spec remote_read(index_node(), inet:socket(), term(), transaction()) -> {ok, term(), transaction()}
+                                                                       | abort()
+                                                                       | socket_error().
+
+remote_read({Partition, _Node}=IndexNode, Socket, Key, Tx = #tx_state{vc_aggr=Aggr,
+                                                                      read_partitions=HasRead}) ->
+
+    ReadRequest = ppb_protocol_driver:read_request(Partition, Key, Aggr, HasRead),
+    ok = gen_tcp:send(Socket, ReadRequest),
+    case gen_tcp:recv(Socket, 0) of
+        {error, Reason} ->
+            {error, Reason};
+        {ok, RawReply} ->
+            case pvc_proto:decode_serv_reply(RawReply) of
+                {error, Aborted} ->
+                    {abort, Aborted};
+                {ok, Value, VersionVC, MaxVC} ->
+                    {ok, Value, update_transacion(IndexNode, VersionVC, MaxVC, Tx)}
+            end
+    end.
+
+%% @doc Update a transaction after a read.
+-spec update_transacion(index_node(), vc(), vc(), transaction()) -> transaction().
+update_transacion(IndexNode, VersionVC, MaxVC, Tx = #tx_state{vc_dep=VCdep,
+                                                              vc_aggr=VCaggr,
+                                                              read_partitions=HasRead}) ->
+    Tx#tx_state{vc_dep = pvc_vclock:max(VersionVC, VCdep),
+                vc_aggr = pvc_vclock:max(MaxVC, VCaggr),
+                read_partitions = sets:add_element(IndexNode, HasRead)}.
+
+
+%%====================================================================
+%% Update Internal functions
+%%====================================================================
+
+%% @doc Check if a key has an assoc value in the given transaction writeset
+%%
+%%      Returns {ok, Value} if it was updated, or {false, Node} if not, where
+%%      Node is the remote Antidote node we should route or read request
+%%
+-spec key_updated(connection(), term(), ws()) -> {ok, term()} | {false, index_node()}.
+key_updated(Conn, Key, WS) ->
+    Node = get_key_indexnode(Conn, Key),
+    NodeWS = get_node_writeset(Node, WS),
+    case orddict:find(Key, NodeWS) of
+        {ok, Value} ->
+            {ok, Value};
+        error ->
+            {false, Node}
+    end.
 
 -spec get_node_writeset(node(), ws()) -> partition_ws().
 get_node_writeset(Node, WS) ->
